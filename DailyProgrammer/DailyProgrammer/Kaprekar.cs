70d368b1916aa0aba798d006b9cbdbf207df6e70
DLong94 <dlong03111994@gmail.com>
2016-11-02 21:26:29 +1000

    Add Directory

---
+++ DailyProgrammer/DailyProgrammer/Kaprekar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyProgrammer
+{
+    class Kaprekar
+    {
+        public void Solve(int start, int end)
+        {
+            List<int> KaprekarNumbers = new List<int>();
+
+            for (int x = start; x <= end; x++)
+            {
+                if (Check(x))
+                {
+                    KaprekarNumbers.Add(x);
+                }
+            }
+            Console.WriteLine(KaprekarNumbers.ToString());
+        }
+
+        private bool Check(int number)
+        {
+            return 1;
+        }
+    }
+}
